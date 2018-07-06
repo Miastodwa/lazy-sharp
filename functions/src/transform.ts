e96b8d7b41cf5e494068a087d41600bbf704fa3c
Jan Franciszek Cieślak <jfcieslak@gmail.com>
2018-07-06 13:53:57 +0200

    size transforms, parms, prefix

--- functions/src/transform.ts
+++ functions/src/transform.ts
@@ -1,8 +1,10 @@
 import * as functions from 'firebase-functions'
 import * as admin from 'firebase-admin'
 import * as Cors from 'cors'
-import {fileUrl} from './utils'
+import {fileUrl, getParams, buidlPipeline, buildPrefix} from './utils'
+import { WriteStreamOptions } from '@google-cloud/storage'
 const cors = Cors({ origin: true })
+
 admin.initializeApp()
 
 export const transform = functions.https.onRequest((req, res) => {
@@ -11,40 +13,69 @@ export const transform = functions.https.onRequest((req, res) => {
 
 	return cors(req, res, async () => {
 
-		const params = {
-			bucket: req.query.bucket,
-			name: req.query.name,
-			width: req.query.width,
-			height: req.query.height,
-			mode: req.query.mode,
-		}
-
-		let nameExt = ''
-		if (params.width) nameExt += `-w:${params.width}`
-		if (params.mode) nameExt += `-mode:${params.mode}`
-
+		const params = getParams(req.query)
+		const prefix = buildPrefix(params.sharp)
+		
 		const storage = admin.storage()
 		const bucket = storage.bucket(params.bucket)
-		const original = bucket.file(params.name)
-		const modified = bucket.file(params.name + nameExt)
+		const original = bucket.file(params.folder + params.name)
+		const modified = bucket.file(params.folder + prefix + params.name)
 
-		// if no nameExt, redirect to original.
-		if (!nameExt) {
+		// if no prefix, redirect to original.
+		if (!prefix) {
 			const [meta] = await original.getMetadata()
 			res.redirect(301, fileUrl(meta))
 		}
 
 		// Check if original and modified files exist
-		const exist = await Promise.all([original.exists(), modified.exists()])
+		const exist = await Promise
+			.all([original.exists(), modified.exists()])
 			.then(v => [v[0][0], v[1][0]])
 
+		// if none exist, 404
+		if (!exist[0] && !exist[1]) {
+			res.status(404)
+			res.send()
+		}
+
 		// if modified exists, redirect to it.
-		if (exist[0] && exist[1]){
+		if (exist[1]) {
 			const [meta] = await modified.getMetadata()
 			res.redirect(301, fileUrl(meta))
 		}
 
-		res.set('Cache-Control', 'public, max-age=600, s-maxage=600')
-		res.send(exist)
+		// if original exists, and modified doesn't, create it, and redirect to it
+		if(exist[0] && !exist[1]){
+			const [meta] = await original.getMetadata()
+			console.log(meta)
+			console.log(original)
+
+			const modifiedMeta: WriteStreamOptions = {
+				public: true,
+				metadata: {
+					contentType: meta.contentType,
+					cacheControl: meta.cacheControl,
+				}
+			}
+			
+			const pipeline = buidlPipeline(params.sharp)
+
+			const fileUploadStream = modified.createWriteStream(modifiedMeta)
+
+			original.createReadStream().pipe(pipeline).pipe(fileUploadStream)
+
+			const streamAsPromise = new Promise((resolve, reject) =>
+				fileUploadStream.on('finish', resolve).on('error', reject)
+			)
+			await streamAsPromise
+				.catch(err => {
+					console.log(err)
+					return res.status(500).send(err)
+				})
+
+			res.set('Cache-Control', 'public, max-age=60, s-maxage=31536000')
+			res.writeHead(200, { contentType: meta.contentType })
+			original.createReadStream().pipe(pipeline).pipe(res)
+		}
 	})
 })
--- functions/src/utils.ts
+++ functions/src/utils.ts
@@ -1,6 +1,59 @@
-const baseUrl = 'https://firebasestorage.googleapis.com/v0/'
+import { TransformParams, SharpParams } from './types'
+import * as sharp from 'sharp'
+
+const baseFirebaseLink = 'https://firebasestorage.googleapis.com/v0/'
 const baseMedialink = 'https://www.googleapis.com/download/storage/v1/'
 
 export function fileUrl(meta: any){
-    return meta.mediaLink.replace(baseMedialink, baseUrl)
+    return meta.mediaLink.replace(baseMedialink, baseFirebaseLink)
+}
+
+export function getParams(q: any ): TransformParams {
+
+    function parseResize(): number[] {
+        const vals = []
+        if (q.width) vals.push(parseInt(q.width))
+        if (q.height) vals.push(parseInt(q.height))
+        return vals
+    }
+
+    function parseFolder(): string {
+        if (!q.folder) return ''
+        if (q.folder.endsWith('/')) return q.folder
+        else return q.folder + '/'
+    }
+
+    const sharpParams: SharpParams = {
+        resize: parseResize(),
+        resizeLimit: q.resizeLimit || 'max',
+        crop: q.crop || false,
+        embed: q.crop ? false : q.embed || false,
+        toFormat: q.toFormat || 'webp'
+    }
+
+    const params: TransformParams = {
+        bucket: q.bucket,
+        folder: parseFolder(),
+        name: q.name,
+        result: q.result || 'url',
+        sharp: sharpParams
+    }
+    return params
+}
+
+export function buidlPipeline( p: SharpParams ) {
+    const pipeline = sharp()
+    if (p.resize) pipeline.resize( p.resize[0], p.resize[1] )[p.resizeLimit]()
+    if (p.crop) pipeline.crop(p.crop)
+    if (p.embed) pipeline.embed()
+    return pipeline.toFormat(p.toFormat)
+}
+
+export function buildPrefix(p: SharpParams): string {
+    if (!p) return null
+    const params = []
+    if (p.resize.length) params.push(`resize:${p.resize.join(',')}`)
+    if (p.crop) params.push(`crop:${p.crop}`)
+    if (p.embed) params.push(`embed:${p.embed}`)
+    return params.length ? params.join('|') + '__' : null
 }
