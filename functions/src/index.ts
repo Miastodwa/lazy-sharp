68a552b2aee9e1d0085d8e10b3109ac4e797ec9b
Jan Franciszek Cieślak <jfcieslak@gmail.com>
2019-05-13 01:05:38 +0200

    streamline responses

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,91 +1,125 @@
 import * as functions from 'firebase-functions'
 import * as admin from 'firebase-admin'
 import * as Cors from 'cors'
-import { fileUrl, getParams, buidlPipeline, buildPrefix, replaceExt } from './utils'
-import { WriteStreamOptions } from '@google-cloud/storage'
-const cors = Cors({ origin: true })
+import { buildPrefix, replaceExt, parseFolder, buidlPipeline } from './utils'
+import { CreateWriteStreamOptions, GetSignedUrlConfig } from '@google-cloud/storage'
+import * as sharp from 'sharp'
 
-admin.initializeApp()
+const cors = Cors({ origin: true })
+const firebaseConfig = JSON.parse(process.env.FIREBASE_CONFIG)
+const CONFIG: GetSignedUrlConfig = {
+	action: 'read',
+	expires: '01-01-2100'
+}
+
+admin.initializeApp({
+	credential: admin.credential.cert(
+		'/Users/franz/Sites/SERVERLESS/lazy-sharp/service_account.json'
+	),
+	databaseURL: firebaseConfig.databaseURL,
+	storageBucket: firebaseConfig.storageBucket,
+	projectId: firebaseConfig.projectId
+})
 
 export const lazysharp = functions.https.onRequest((req, res) => {
 
-    if (req.method !== 'GET') return res.status(403).send('Forbidden')
+	if (req.method !== 'GET') return res.status(405).send('Method Not Allowed')
 
-    return cors(req, res, async () => {
+	return cors(req, res, async () => {
 
-        const params = getParams(req.query)
-        const prefix = buildPrefix(params.sharp)
+		const {query} = req
 
-        const storage = admin.storage()
-        const bucket = storage.bucket(params.bucket)
-        const original = bucket.file(params.folder + params.name)
-        const modifiedName = replaceExt(params.name, params.sharp.toFormat)
-        const modified = bucket.file(params.folder + prefix + modifiedName)
+		const resizeOptions: sharp.ResizeOptions = {
+			width: +query.width || null,
+			height: +query.height || null,
+			fit: query.fit,
+			position: query.position,
+			background: query.background,
+			withoutEnlargement: query.withoutEnlargement
+		}
+		const params = {
+			bucket: query.bucket,
+			folder: parseFolder(query.folder),
+			name: query.name,
+			result: query.result || 'redirect',
+			resizeOptions: resizeOptions,
+			format: query.format
+		}
+		const prefix = buildPrefix(resizeOptions)
+		const modifiedName = replaceExt(params.name, params.format)
 
-        // if no prefix, redirect to original.
-        if (!prefix) {
-            const [meta] = await original.getMetadata()
-            if (params.result === 'redirect') res.redirect(301, fileUrl(meta))
-            if (params.result === 'stream') res.redirect(301, fileUrl(meta))
-            if (params.result === 'url') res.send({ mediaLink: fileUrl(meta) })
-        }
+		const storage = admin.storage()
+		const bucket = storage.bucket(params.bucket)
+		const original = bucket.file(params.folder + params.name)
+		const modified = bucket.file(params.folder + prefix + '__' + modifiedName)
 
-        // Check if original and modified files exist
-        const exist = await Promise
-            .all([original.exists(), modified.exists()])
-            .then(v => [v[0][0], v[1][0]])
+		// if no prefix, redirect to original.
+		if (!prefix && modifiedName === params.name) {
+			const url = await original
+				.getSignedUrl(CONFIG)
+				.catch(e => res.status(500).send(e.message))
 
-        //________________________________________
+			if (params.result === 'redirect') res.redirect(301, url[0])
+			if (params.result === 'stream') res.redirect(301, url[0])
+			if (params.result === 'url') res.send({ url: url[0] })
+			return
+		}
 
-        // if none exist, 404
-        if (!exist[0] && !exist[1]) {
-            console.log('not found')
-            res.status(404)
-            res.send()
-        }
 
-        // if modified exists, redirect to it.
-        if (exist[1]) {
-            console.log('modified exists')
-            const [meta] = await modified.getMetadata()
-            if (params.result === 'redirect') res.redirect(301, fileUrl(meta))
-            if (params.result === 'stream') res.redirect(301, fileUrl(meta))
-            if (params.result === 'url') res.send({ mediaLink: fileUrl(meta) })
-        }
+		// if modified exists, redirect to it.
+		const [[modifiedExists], [modifiedUrl]] = await Promise.all([
+			modified.exists(),
+			modified.getSignedUrl(CONFIG)
+		])
+		console.log(modifiedExists)
+		if (modifiedExists) {
+			console.log('modified exists')
+			if (params.result === 'redirect') res.redirect(301, modifiedUrl)
+			if (params.result === 'stream') res.redirect(301, modifiedUrl)
+			if (params.result === 'url') res.send({ url: modifiedUrl })
+			return
+		}
 
-        // if original exists, and modified doesn't, create it, and redirect to it
-        if (exist[0] && !exist[1]) {
-            console.log('creating file...')
-            const [originalMeta] = await original.getMetadata()
+		// if original exists, and modified doesn't, create it, and redirect to it
+		console.log('creating file...')
+		const [originalMeta] = await original.getMetadata()
 
-            const modifiedMeta: WriteStreamOptions = {
-                public: true,
-                metadata: {
-                    contentType: `image/${params.sharp.toFormat}`,
-                    cacheControl: originalMeta.cacheControl,
-                }
-            }
+		const modifiedMeta: CreateWriteStreamOptions = {
+			public: true,
+			metadata: {
+				contentType: `image/${params.format}`,
+				cacheControl: originalMeta.cacheControl,
+			}
+		}
+		console.log(originalMeta.cacheControl)
 
-            const pipeline = buidlPipeline(params.sharp)
+		const pipeline = await buidlPipeline(resizeOptions, params.format)
+			.catch( e => res.status(422).send(e.message))
+		if (!pipeline) return
 
-            const fileUploadStream = modified.createWriteStream(modifiedMeta)
+		if (params.result === 'stream') {
+			try {
+				original.createReadStream().pipe(pipeline).pipe(res)
+				return
+			}
+			catch (error) {
+				res.status(500).send(error)
+			}
+		}
 
-            if (params.result === 'stream') {
-                original.createReadStream().pipe(pipeline).pipe(res)
-                return
-            }
+		const fileUploadStream = modified.createWriteStream(modifiedMeta)
+		original.createReadStream().pipe(pipeline).pipe(fileUploadStream)
 
-            original.createReadStream().pipe(pipeline).pipe(fileUploadStream)
+		const promiseUpload = new Promise((resolve, reject) =>
+			fileUploadStream
+				.on('finish', resolve)
+				.on('error', reject)
+		)
+		await promiseUpload
+		console.log('...file created', modifiedUrl)
 
-            const promiseUpload = new Promise((resolve, reject) =>
-                fileUploadStream.on('finish', resolve).on('error', reject)
-            )
-            await promiseUpload.catch(err => res.status(500).send(err))
-            console.log('...file created')
-
-            const [meta] = await modified.getMetadata()
-            if (params.result === 'redirect') res.redirect(301, fileUrl(meta))
-            if (params.result === 'url') res.send({ mediaLink: fileUrl(meta) })
-        }
-    })
+		if (params.result === 'redirect') res.redirect(301, modifiedUrl)
+		if (params.result === 'url') res.send({ url: modifiedUrl })
+		return
+	})
 })
