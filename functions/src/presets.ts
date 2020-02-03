e85736ebe805383b7657cbc8d5584fdf451ffd69
JF Cieslak <jf.cieslak@miastodwa.org>
2020-02-03 18:31:28 +0100

    update presets

--- functions/src/presets.ts
+++ functions/src/presets.ts
@@ -2,6 +2,17 @@ import { Presets } from './@types'
 
 const presets: Presets = {
 	default: {},
+	portfolio: {
+		width: 560,
+		height: 420,
+		format: 'webp',
+	},
+	full_size: {
+		width: 960,
+		height: 720,
+		fit: 'inside',
+		format: 'webp',
+	},
 	map_preview: {
 		width: 160,
 		height: 104,
@@ -10,6 +21,16 @@ const presets: Presets = {
 	profile_roll: {
 		width: 240,
 		height: 160,
+		format: 'webp',
+	},
+	list_item: {
+		width: 300,
+		height: 200,
+		format: 'webp',
+	},
+	micro: {
+		width: 30,
+		height: 20,
 		format: 'webp',
 	},
 }
